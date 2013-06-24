1fdc9c20bafa130fb6322f23cfb7f4ef1783f3cc
Shane Todhunter <shane@delta-biz.com>
2013-06-23 21:55:01 -0700

    Added login from Example

---
+++ module/Auth/src/Auth/Controller/SuccessController.php
@@ -0,0 +1,20 @@
+<?
+//module/Auth/src/Auth/Controller/SuccessController.php
+namespace Auth\Controller;
+ 
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+ 
+class SuccessController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        if (! $this->getServiceLocator()
+                 ->get('AuthService')->hasIdentity()){
+            return $this->redirect()->toRoute('login');
+        }
+         
+        return new ViewModel();
+    }
+}
+?>
